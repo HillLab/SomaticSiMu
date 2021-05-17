c6c7c44a84734944760076129c4edc8bd6a4c193
davidchen0420 <57333478+davidchen0420@users.noreply.github.com>
2021-05-17 14:33:04 -0400

    final package production commit

--- setup.py
+++ setup.py
@@ -5,20 +5,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="SomaticSiMu", 
-    version="2.0.0",
+    version="3.0.0",
     author="David Chen",
     author_email="dchen362@uwo.ca",
     description="Mutational Signature Simulation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=["pandas", "numpy"],
+    install_requires=["pandas", "numpy", "tqdm", "pillow"],
     url="https://github.com/HillLab/SomaticSiMu",
     packages=setuptools.find_packages(),
     classifiers=[
 	"Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
-        "License :: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
-Public License",
+        "License :: OSI Approved :: Common Public License",
         "Operating System :: OS Independent",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
